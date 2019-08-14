5c4fa5383256872095304340325214e0aded4ffb
Camilo Andres Concha Agudelo <cconcha@belatrix.com>
2019-08-14 15:17:05 -0500

    added the link component and the prettier config for dev

--- tooltip.js
+++ tooltip.js
@@ -1,25 +1,42 @@
 class Tooltip extends HTMLElement {
-  constructor() {
-    super();
-    this._tootipContainer;
-  }
+    constructor() {
+        super();
+        this._tootipContainer;
+        this._tootipText = 'Dummy Text';
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `
+        <slot></slot>
+        <span>(?)</span>
+        `;
+    }
 
-  connectedCallback() {
-    const tooltipIcon = document.createElement("span");
-    tooltipIcon.textContent = " (?)";
-    tooltipIcon.addEventListener("mouseenter", this._showTooltip);
-    tooltipIcon.addEventListener("mouseleave", this._hideTootip);
-    this.appendChild(tooltipIcon);
-  }
+    connectedCallback() {
+        if (this.hasAttribute('text')) {
+            this._tootipText = this.getAttribute('text');
+        }
 
-  _showTooltip() {
-    this._tooltipContainer = document.createElement("div");
-    this._tooltipContainer.textContent = "This is the tooltip text!";
-    this.appendChild(this._tooltipContainer);
-  }
+        const tooltipIcon = this.shadowRoot.querySelector('span');
+        tooltipIcon.textContent = ' (?)';
+        tooltipIcon.addEventListener(
+            'mouseenter',
+            this._showTooltip.bind(this)
+        );
+        tooltipIcon.addEventListener('mouseleave', this._hideTootip.bind(this));
+        this.shadowRoot.appendChild(tooltipIcon);
+    }
 
-  _hideTootip() {
-      this.removeChild(this._tooltipContainer);
-  }
+    _showTooltip() {
+        this._tooltipContainer = document.createElement('div');
+        this._tooltipContainer.textContent = this._tootipText;
+        this._tooltipContainer.style.backgroundColor = 'black';
+        this._tooltipContainer.style.position = 'absolute';
+        this._tooltipContainer.style.zIndex = '10';
+        this._tooltipContainer.style.color = 'white';
+        this.shadowRoot.appendChild(this._tooltipContainer);
+    }
+
+    _hideTootip() {
+        this.shadowRoot.removeChild(this._tooltipContainer);
+    }
 }
-customElements.define("my-tooltip", Tooltip);
+customElements.define('my-tooltip', Tooltip);
